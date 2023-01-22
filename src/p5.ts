4df9bb728447f7c77e308f36f1b6814f5ea15ecc
Sato Masahiro <satomasahiro2005@gmail.com>
2023-01-23 07:00:24 +0900

    🦄 refactor: p5.tsがコピペのものだったので修正
    p5.jsをtypescriptで使う方法の記事に載っていたコードを消し忘れていたので削除

--- src/p5.ts
+++ src/p5.ts
@@ -1,20 +1,7 @@
 import p5 from "p5";
 
 const sketch = (p: p5) => {
-  // 描画色1・2
-  const color1 = p.color("#fffbe3");
-  const color2 = p.color("#24495c");
-  // 描画色1の強さ
-  let color1amount = 1;
-
-  /** 初期化処理 */
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.noStroke(); // 線なし（塗りつぶしのみ）に設定
-    p.background("#eeeeee"); // 背景色を設定
-  };
-
-  /** フレームごとの描画処理 */
+  p.setup = () => {};
   p.draw = () => {};
 };
 
